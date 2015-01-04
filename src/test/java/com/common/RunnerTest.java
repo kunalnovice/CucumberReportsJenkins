18fed18e9fb0d6e3a14469ae4607443e1cfa0c21
kunalnovice <kunalmehta.km@gmail.com>
2015-01-04 12:10:15 +0000

    changes in RunnerTest to generate Json

--- src/test/java/com/common/RunnerTest.java
+++ src/test/java/com/common/RunnerTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/com/features", glue = "com.stepdefs", 
                  tags = { "@runner" }, monochrome= true ,
-                 plugin = {"pretty", "html:target/cukes/mehta" })
+                 plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json" })
 public class RunnerTest {
 
 }
